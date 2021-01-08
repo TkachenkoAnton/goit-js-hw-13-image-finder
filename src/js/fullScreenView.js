244379f7dbadf4f5bdc96de5bd51664c02947c37
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-01-08 10:44:46 +0200

    update markup

--- src/js/fullScreenView.js
+++ src/js/fullScreenView.js
@@ -4,9 +4,7 @@ import * as basicLightbox from 'basiclightbox';
 function fullScreenView(e) {
   if (e.target.nodeName === 'BUTTON') {
     basicLightbox
-      .create(
-        `<img class="modalImg" width="1400" height="900" src="https://placehold.it/1400x900">`,
-      )
+      .create(`<img class="modalImg" src="https://placehold.it/1400x900">`)
       .show();
     const modalImg = document.querySelector('.modalImg');
     modalImg.src = e.target.dataset.source;
