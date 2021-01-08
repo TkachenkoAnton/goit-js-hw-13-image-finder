317ba7a91cfad28c0eeceae8df1518d511644499
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2021-01-08 17:17:38 +0200

    add cdn bootstrap

--- src/index.js
+++ src/index.js
@@ -6,7 +6,3 @@ import './js/finalRender';
 import './js/fullScreenView';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import 'material-design-icons/iconfont/material-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/js/bootstrap.esm.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
